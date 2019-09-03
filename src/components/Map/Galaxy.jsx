d01e91a1f9fa0496df52867a050255e1c0abb9b0
Ivan Delic <ivan.delic@gmail.com>
2019-09-03 17:31:23 +0200

    working on the LCARS dashboard

---
+++ src/components/Map/Galaxy.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Galaxy extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Galaxy!</h1>
+            </div>
+        );
+    }
+}
+
+export default Galaxy;
---
+++ src/screens/Dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../../images/logo.svg';
+import galaxy from '../../images/galaxy.svg';
+import './Dashboard.css';
+
+function Dashboard() {
+  return (
+    <div className="dashboard">
+      <div className="lcars-wrapper"></div>
+    </div>
+  );
+}
+
+export default Dashboard;
